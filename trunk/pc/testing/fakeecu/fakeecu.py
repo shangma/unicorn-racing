514d822b24d163013ae1848ecc1777f6398cc05c
jbaekdahl <jbaekdahl@cee7091a-c234-0410-ab42-b1a86514ffa4>
2009-04-18 20:27:12 +0000

    Så skulle repository være godt igen... Måske

---
+++ trunk/pc/testing/fakeecu/fakeecu.py
@@ -0,0 +1,31 @@
+import serial
+from string import strip
+from binascii import hexlify, unhexlify
+from time import sleep
+
+tty=serial.Serial("/dev/ttyUSB0",19200,timeout=0.35)
+datalogfile=open("testdatalog.txt","r")
+
+while 1:
+	ttydata=hexlify(tty.read(10))
+	print ttydata
+	if ttydata=="12345678170800000000":
+		print ttydata
+		data=strip(datalogfile.readline(),"\n\r")
+		if len(data)==0:
+			print "EOF reached\n"
+			datalogfile.close
+			datalogfile=open("testdatalog.txt")
+			print "File reopened\n"
+		tty.flushOutput()
+		tty.write(unhexlify(data))
+		print "Sent:"
+		print data
+		tty.flushInput()
+
+	else:
+		print "No data received\n"
+		tty.flushInput()
+		tty.flushOutput()
+
+datalogfile.close
