8c69c212c8d51c281c6e807066103139c3e559bc
spyfluen_mj@hotmail.com <spyfluen_mj@hotmail.com@cee7091a-c234-0410-ab42-b1a86514ffa4>
2010-01-27 13:12:59 +0000

    virker

--- trunk/avr/CAN/node1/node1.c
+++ trunk/avr/CAN/node1/node1.c
@@ -13,9 +13,7 @@
 #define NB_TARGET   1			// Antal noder
 
 extern void display_sensor_values(void);
-
-U8 response_buffer[9];
-st_cmd_t response_msg;
+extern void req_sensor_data(U8 pakke, U8 node);
 
 //------------------------------------------------------------------------------
 //! This program performs some remote frames (c.f. NB_TARGET).
@@ -46,25 +44,13 @@ int main (void)
     CANIE2 = 0xFF;
     CANIE1 = 0xFF;
 
-    CANGIE |=(1<<ENRX);
+    //CANGIE |=(1<<ENRX);
     //CANGIE |=(1<<ENTX);
 
     CANGIE |=(1<<ENIT);
 
-    response_msg.pt_data = &response_buffer[0];
-    response_msg.status = 0;
 
-    // --- Init Rx Commands
-    for(i=0; i<9; i++) response_buffer[i]=0; // Nulstiller buffer
-    response_msg.id.std = ID_TAG_BASE + j;
-    response_msg.ctrl.ide = 0;
-    response_msg.ctrl.rtr = 0;
-    response_msg.dlc = 5;
-    response_msg.cmd = CMD_RX_DATA_MASKED;
-    
-    uart_mini_printf("GOGO");
-    while(1);
-    //display_sensor_values();
+    display_sensor_values();
 
     return 0;
 }
@@ -81,13 +67,14 @@ int main (void)
 void display_sensor_values(void)
 {
     U8 i, j=0;
- 
-    U8 tx_remote_buffer[9];
-    st_cmd_t tx_remote_msg;
+    U8 k = 0;
+
+    U8 response_buffer[9];
+    st_cmd_t response_msg;
 
     // --- Init variables
-    tx_remote_msg.pt_data = &tx_remote_buffer[0];
-    tx_remote_msg.status = 0;
+    response_msg.pt_data = &response_buffer[0];
+    response_msg.status = 0;
 
     // UART
     uart_mini_printf("GOGO");
@@ -97,35 +84,67 @@ void display_sensor_values(void)
         wait_for(100);  // x ms between refreshed screen
         for(j=0; j<NB_TARGET; j++)
         {
-            CANGIE |=(1<<ENIT);
+            //CANGIE |=(1<<ENIT);
             // --- Init Rx Commands
       	    for(i=0; i<9; i++) response_buffer[i]=0; // Nulstiller buffer
             response_msg.id.std = ID_TAG_BASE + j;
             response_msg.ctrl.ide = 0;
             response_msg.ctrl.rtr = 0;
-            response_msg.dlc = 5;
+            response_msg.dlc = 8;
             response_msg.cmd = CMD_RX_DATA_MASKED;
             // --- Rx Command
             while(can_cmd(&response_msg) != CAN_CMD_ACCEPTED);
 
-            // --- Init Tx Commands
-  	        for(i=0; i<9; i++) tx_remote_buffer[i]=0; // Nulstiller buffer
-            tx_remote_msg.id.std = ID_TAG_BASE + j;
-            tx_remote_msg.ctrl.ide = 0;
-            tx_remote_msg.ctrl.rtr = 1;
-            tx_remote_msg.dlc = 8; // Antal data bytes der skal modtages 
-            tx_remote_msg.cmd = CMD_TX_REMOTE;
-            // --- Tx Command
-            while(can_cmd(&tx_remote_msg) != CAN_CMD_ACCEPTED);
+	    req_sensor_data(2,3);
 
-            // --- Wait for Tx remote completed
-            while(can_get_status(&tx_remote_msg) == CAN_STATUS_NOT_COMPLETED);
+            wait_for(10); // Wait x ms for a response if exits
 
-            wait_for(50); // Wait x ms for a response if exits
+            if (can_get_status(&response_msg) == CAN_STATUS_COMPLETED){
+                // --- Node ID
+		uart_mini_printf("Node: %d",response_msg.id.std-127);
+                
+                // --- Data               
+               	uart_mini_printf(", Data1: %03d", response_buffer[0]);
+
+		uart_mini_printf(", Data2: %03d", response_buffer[1]);
+		uart_mini_printf(", Data3: %03d", response_buffer[2]);
+		uart_mini_printf(", Data4: %03d", response_buffer[3]);
+		uart_mini_printf(", Data5: %03d", response_buffer[4]);
+		uart_mini_printf(", Data6: %03d", response_buffer[5]);
+		uart_mini_printf(", Data7: %03d", response_buffer[6]);                
+		uart_mini_printf(", Data8: %03d", response_buffer[7]);
+		uart_mini_printf("\r\n");  
+            }else{
+                response_msg.cmd = CMD_ABORT;
+          	    while (can_cmd(&response_msg) != CAN_CMD_ACCEPTED);
+            }
         }
     }
 }
 
+void
+req_sensor_data(U8 pakke, U8 node){
+	U8 tx_remote_buffer[9];
+    	st_cmd_t tx_remote_msg;
+
+	tx_remote_msg.pt_data = &tx_remote_buffer[0];
+	tx_remote_msg.status = 0;
+	
+	tx_remote_buffer[0]=pakke; // Nulstiller buffer
+
+        tx_remote_msg.id.std = ID_TAG_BASE+node;
+        tx_remote_msg.ctrl.ide = 0;
+        tx_remote_msg.ctrl.rtr = 1;
+        tx_remote_msg.dlc = 8; // Antal data bytes der skal modtages 
+        tx_remote_msg.cmd = CMD_TX_DATA;
+        // --- Tx Command
+        while(can_cmd(&tx_remote_msg) != CAN_CMD_ACCEPTED);
+
+	// --- Wait for Tx remote completed
+        while(can_get_status(&tx_remote_msg) == CAN_STATUS_NOT_COMPLETED);
+}
+
+/*
 ISR(CANIT_vect)
 {
     U8 i,j=0;
@@ -159,4 +178,4 @@ ISR(CANIT_vect)
   	    while (can_cmd(&response_msg) != CAN_CMD_ACCEPTED);
     }
 }
-
+*/
