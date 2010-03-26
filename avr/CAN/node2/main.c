99734507fbefc70baa2bd81f868d1c03df67800a
spyfluen_mj@hotmail.com <spyfluen_mj@hotmail.com@cee7091a-c234-0410-ab42-b1a86514ffa4>
2010-03-26 22:58:36 +0000

    Lavet så det er muligt at styre styrken af rpm leds

--- avr/CAN/node2/main.c
+++ avr/CAN/node2/main.c
@@ -2,11 +2,12 @@
 #include "config.h"
 #include "can_lib.h"
 #include <util/delay.h>
+#include "led.h"
 
 //_____ D E F I N I T I O N S __________________________________________________
 #define ID_BASE 0x80
 #define NODE 3
-
+#define RPM_LED_POWER 255 /* on in RPM_LED_POWER/255 % of the time */
 //_____ D E C L A R A T I O N S ________________________________________________
 extern void display_sensor_values(void);
 void init(void);
@@ -30,20 +31,26 @@ int main (void)
     int i=0;
     CLKPR = 0x80;  CLKPR = 0x00;  // Clock prescaler Reset
 
-    // Init CAN, UART, I/O
+ //    Init CAN, UART, I/O
     init();
 
+    /* Init timer for led power interrupt */
+    TIMSK0 |= 1<<TOIE0 | 1<<OCIE0A;
+    TCCR0A |= 1<<CS02;
+    OCR0A = RPM_LED_POWER;
+    sei();
+
     DDRE = 0xFF;
-    PORTE = 0xFF;
+    //PORTE = 0xFF;
 
     DDRB = 0xFF;
-    PORTB = 0xFF;
+    //PORTB = 0xFF;
 
     DDRC = 0xFF;
-    PORTC = 0xFF;
+    //PORTC = 0xFF;
 
     DDRA = 0b11100000;
-    PORTA = 0xFF;
+    //PORTA = 0xFF;
     // --- Init variables
     tx_remote_msg.pt_data = &tx_remote_buffer[0];
     tx_remote_msg.status = 0;
@@ -54,13 +61,13 @@ int main (void)
     while (1)
     {
         // Venter på datakald
-        dataType = wait_CAN_request();
+//        dataType = wait_CAN_request();
         for (i=0;i<9;i++){
                 _delay_ms(100);
-                PORTE = 1<<i;
+                LED_REG1 = 7<<i;
         }
         for (i=0;i<9;i++){
-                PORTB = 1<<i;
+                LED_REG2 = 7<<i;
                 _delay_ms(100);
         }
         for (i=0;i<8;i++){
@@ -71,11 +78,34 @@ int main (void)
                 PORTA = 1<<i;
                 _delay_ms(100);
         }
-        // Sætter udgående sensordata
-        set_sensor_data(dataType);
+        PORTC = SEG_1;
+        _delay_ms(300);
+        PORTC = SEG_2;
+        _delay_ms(300);
+        PORTC = SEG_3;
+        _delay_ms(300);
+        PORTC = SEG_4;
+        _delay_ms(300);
+        PORTC = SEG_5;
+        _delay_ms(300);
+        PORTC = SEG_6;
+        _delay_ms(300);
+        PORTC = SEG_7;
+        _delay_ms(300);
+        PORTC = SEG_8;
+        _delay_ms(300);
+        PORTC = SEG_9;
+        _delay_ms(300);
+        
+        LED15_ON;
+        LED14_ON;
+        _delay_ms(2000);
+        LED16_ON;
+    // Sætter udgående sensordata
+//        set_sensor_data(dataType);
 
         // Transmittere sensordata
-        CAN_transmit(); 
+//        CAN_transmit(); 
     }
 
     return 0;
