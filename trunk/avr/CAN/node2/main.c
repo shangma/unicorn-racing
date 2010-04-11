670faad5c367507e0a0c426fe69b8130d38261f4
spyfluen_mj@hotmail.com <spyfluen_mj@hotmail.com@cee7091a-c234-0410-ab42-b1a86514ffa4>
2010-04-11 12:45:27 +0000

    Så skulle svn node2 kode gerne være up to date

--- trunk/avr/CAN/node2/main.c
+++ trunk/avr/CAN/node2/main.c
@@ -3,111 +3,124 @@
 #include "can_lib.h"
 #include <util/delay.h>
 #include "led.h"
+#include "../lib/can_defs.h"
 
 //_____ D E F I N I T I O N S __________________________________________________
 #define ID_BASE 0x80
-#define NODE 3
-#define RPM_LED_POWER 255 /* on in RPM_LED_POWER/255 % of the time */
 //_____ D E C L A R A T I O N S ________________________________________________
-extern void display_sensor_values(void);
 void init(void);
-unsigned short int wait_CAN_request(void);
-void CAN_transmit(void);
-void set_sensor_data(unsigned short int dataType);
+unsigned short int can_check( U8 msg_id);
+unsigned short int can_update_rx_msg(st_cmd_t* msg, U8 msg_id, U8 data_length);
 
 //_____ VARIABLES ______________________________________________________________
-unsigned short int dataType = 0;
-
-// Transmit buffer
-U8 tx_remote_buffer[8];
-st_cmd_t tx_remote_msg;
-
 // Recieve buffer
-U8 response_buffer[8];
-st_cmd_t response_msg;
+U8 rpm_response_buffer[8];
+st_cmd_t rpm_msg;
+
+U8 gear_status_response_buffer[8];
+st_cmd_t gear_status_msg;
 
 int main (void)
 {	
-    int i=0;
+    U8 i=0;
+    int j=0;
+    int k=12;
     CLKPR = 0x80;  CLKPR = 0x00;  // Clock prescaler Reset
 
- //    Init CAN, UART, I/O
+//  Init CAN, UART, I/O
     init();
 
-    /* Init timer for led power interrupt */
-    TIMSK0 |= 1<<TOIE0 | 1<<OCIE0A;
-    TCCR0A |= 1<<CS02;
-    OCR0A = RPM_LED_POWER;
+    init_led();
     sei();
 
-    DDRE = 0xFF;
-    //PORTE = 0xFF;
-
-    DDRB = 0xFF;
-    //PORTB = 0xFF;
-
-    DDRC = 0xFF;
-    //PORTC = 0xFF;
-
-    DDRA = 0b11100000;
-    //PORTA = 0xFF;
     // --- Init variables
-    tx_remote_msg.pt_data = &tx_remote_buffer[0];
-    tx_remote_msg.status = 0;
+    rpm_msg.pt_data = &rpm_response_buffer[0];
+    rpm_msg.status = 0;
 
-    response_msg.pt_data = &response_buffer[0];
-    response_msg.status = 0;
+    gear_status_msg.pt_data = &gear_status_response_buffer[0];
+    gear_status_msg.status = 0;
 
+    fade_in(5000, 50);
+    SEG_0;
+    
+    can_update_rx_msg(&rpm_msg, rpm_msgid, 8);
+    can_update_rx_msg(&gear_status_msg, gear_status_msgid, 1);   
     while (1)
     {
-        // Venter på datakald
-//        dataType = wait_CAN_request();
-        for (i=0;i<9;i++){
-                _delay_ms(100);
-                LED_REG1 = 7<<i;
-        }
-        for (i=0;i<9;i++){
-                LED_REG2 = 7<<i;
-                _delay_ms(100);
-        }
-        for (i=0;i<8;i++){
-                PORTC = 1<<i;
-                _delay_ms(100);
-        }
-        for (i=5;i<9;i++){
-                PORTA = 1<<i;
-                _delay_ms(100);
-        }
-        PORTC = SEG_1;
-        _delay_ms(300);
-        PORTC = SEG_2;
-        _delay_ms(300);
-        PORTC = SEG_3;
-        _delay_ms(300);
-        PORTC = SEG_4;
-        _delay_ms(300);
-        PORTC = SEG_5;
-        _delay_ms(300);
-        PORTC = SEG_6;
-        _delay_ms(300);
-        PORTC = SEG_7;
-        _delay_ms(300);
-        PORTC = SEG_8;
-        _delay_ms(300);
-        PORTC = SEG_9;
-        _delay_ms(300);
-        
-        LED15_ON;
-        LED14_ON;
-        _delay_ms(2000);
-        LED16_ON;
-    // Sætter udgående sensordata
-//        set_sensor_data(dataType);
-
-        // Transmittere sensordata
-//        CAN_transmit(); 
+            if (can_get_status(&rpm_msg) == CAN_STATUS_COMPLETED) {  // check for rpm_msg
+                    i = rpm_response_buffer[0];                     
+                    can_update_rx_msg(&rpm_msg, rpm_msgid, 8);      // update rpm_msg to accept a new msg
+                    if (i == 0) {
+                            if (j>0) {
+                                    j--;
+                                    if (j<8)
+                                            LED_REG1 ^=  (1<<j);
+                                    else
+                                            LED_REG2 ^=  (1<<(j-8));
+                            }
+                    }
+                    else if (i == 1) {
+                            if (j<16) {
+                                    if (j<8)
+                                            LED_REG1 |= (1<<j);
+                                    else
+                                            LED_REG2 |= (1<<(j-8));
+                                    j++;
+                            }
+                    }
+                    else if (i == 2) {
+                            if (k<=11) {
+                                    k++;
+                                    OCR0A = k*20;
+                            }
+                    }
+                    else if (i == 3) {
+                            if (k>1) {
+                                    k--;
+                                    OCR0A = k*20;
+                            }
+                            if (k==1) 
+                                    OCR0A = 2;
+                    }
+            }
+            if (can_get_status(&gear_status_msg) == CAN_STATUS_COMPLETED) {  // check for gear_status_msg
+                    i = gear_status_response_buffer[0];                     
+                    can_update_rx_msg(&gear_status_msg, gear_status_msgid, 1);      // update gear_status_msg to accept a new msg
+ 
+                    switch (i) {
+                            case 0:
+                                    SEG_0;
+                                    break;
+                            case 1:
+                                    SEG_1;
+                                    break;
+                            case 2:
+                                    SEG_2;
+                                    break;
+                            case 3:
+                                    SEG_3;
+                                    break;
+                            case 4:
+                                    SEG_4;
+                                    break;
+                            case 5:
+                                    SEG_5;
+                                    break;
+                            case 6:
+                                    SEG_6;
+                                    break;
+                            case 7:
+                                    SEG_7;
+                                    break;
+                            case 8:
+                                    SEG_8;
+                                    break;
+                            case 9:
+                                    SEG_9;
+                                    break;
+                    }
+            }
     }
-
     return 0;
 }
 
@@ -120,53 +133,39 @@ void init(void)
     DDRA = 0xFF;
 }
 
-unsigned short int wait_CAN_request(void)
+unsigned short int can_check( U8 msg_id)
 {
     unsigned short int i = 0;
     // --- Init Rx Commands        
     for(i=0; i<8; i++)  // Nulstiller buffer
-        response_buffer[i]=0;
+        rpm_response_buffer[i]=255;
 
-    response_msg.id.std = ID_BASE + NODE;
-    response_msg.ctrl.ide = 0;
-    response_msg.dlc = 1;               //Antal bytes der skal modtages
-    response_msg.cmd = CMD_RX_DATA_MASKED;
-   
+    rpm_msg.id.std = msg_id;
+    rpm_msg.ctrl.ide = 0;
+    rpm_msg.ctrl.rtr = 0;
+    rpm_msg.dlc = 8;               //Antal bytes der skal modtages
+    rpm_msg.cmd = CMD_RX_DATA_MASKED;
     // --- Rx Command
-    while(can_cmd(&response_msg) != CAN_CMD_ACCEPTED);
-
+    while(can_cmd(&rpm_msg) != CAN_CMD_ACCEPTED);
     // Venter på data er modtaget
-    while(can_get_status(&response_msg) == CAN_STATUS_NOT_COMPLETED);
+    while(can_get_status(&rpm_msg) == CAN_STATUS_NOT_COMPLETED); 
 
-return response_buffer[0];}
-
-void CAN_transmit(void)
-{
-    tx_remote_msg.id.std = ID_BASE;
-    tx_remote_msg.ctrl.ide = 0;
-    tx_remote_msg.ctrl.rtr = 1;
-    tx_remote_msg.dlc = 8;
-    tx_remote_msg.cmd = CMD_TX_DATA;
-
-    // --- Tx Command
-    while(can_cmd(&tx_remote_msg) != CAN_CMD_ACCEPTED);
-
-    // --- Wait for Tx remote completed
-    while(can_get_status(&tx_remote_msg) == CAN_STATUS_NOT_COMPLETED);   
+return rpm_response_buffer[0];
 }
 
-void set_sensor_data(unsigned short int dataType)
+unsigned short int can_update_rx_msg(st_cmd_t* msg, U8 msg_id, U8 dlc)
 {
-    unsigned short int i = 0;
+        U8 i;
+    
+        for (i=0; i<dlc; i++) {
+                *(msg->pt_data+i) = 255;
+        }
+        
+        msg->id.std = msg_id;
+        msg->ctrl.ide = 0;
+        msg->ctrl.rtr = 0;
+        msg->dlc = dlc;
+        msg->cmd = CMD_RX_DATA_MASKED;
 
-    // --- Init Tx Commands
-    for(i=0; i<8; i++) 
-        tx_remote_buffer[i]=0;
-
-    if(dataType == 0)
-        tx_remote_buffer[0] = 112;
-    else if(dataType == 1)
-        tx_remote_buffer[0] = 155;
-    else if(dataType == 2)
-        tx_remote_buffer[0] = 44;
+        while(can_cmd(msg) != CAN_CMD_ACCEPTED);
 }
