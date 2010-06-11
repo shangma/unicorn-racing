1d7f5d76c82a2c8ab0bf25a9f3e300921d30e36c
spyfluen_mj@hotmail.com <spyfluen_mj@hotmail.com@cee7091a-c234-0410-ab42-b1a86514ffa4>
2010-06-11 12:42:31 +0000

    Sender EcuCommError til display som sætte 7-seg til E hvis der er en error

--- avr/CAN/node2/can_func.h
+++ avr/CAN/node2/can_func.h
@@ -6,6 +6,9 @@ st_cmd_t rpm_msg;
 U8 gear_status_response_buffer[8];
 st_cmd_t gear_status_msg;
 
+U8 error_response_buffer[8];
+st_cmd_t error_msg;
+
 /* function to check for new msg with msg_id */
 unsigned short int can_check( U8 msg_id);
 
