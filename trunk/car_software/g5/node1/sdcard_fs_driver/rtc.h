65767a22e97ff4ce835ef210f48dede56cef877d
spyfluen_mj@hotmail.com <spyfluen_mj@hotmail.com@cee7091a-c234-0410-ab42-b1a86514ffa4>
2011-01-06 01:24:05 +0000

    Funktionerne til at skrive til RTC er blevet delt i dem der har med TWI at gøre og dem der kun skal bruges til RTC. Filer oprettet på sdkort får nu det rigtige tidsstempel.

--- trunk/car_software/g5/node1/sdcard_fs_driver/rtc.h
+++ trunk/car_software/g5/node1/sdcard_fs_driver/rtc.h
@@ -10,8 +10,24 @@ typedef struct {
 	BYTE	sec;	/* 0..59 */
 } RTC;
 
-BOOL iic_write (BYTE, UINT, UINT, const void*);	/* Write to IIC device */
-BOOL iic_read (BYTE, UINT, UINT, void*);		/* Read from IIC device */
+/* Clears halt bit in the RTC. See datasheet */
+BOOL rtc_clearHaltBit(void);
+
+/* Able to write bytes to the rtc */
+BOOL RTC_write(
+	char dev,		/* Device address */
+	uint8_t adr,		/* Write start address */
+	uint8_t cnt,		/* Write byte count */
+	uint8_t *buff		/* Write data buffer */
+);
+
+/* Able to read bytes from the rtc */
+BOOL RTC_read(
+	char dev,		/* Device address */
+	uint8_t adr,		/* Read start address */
+	uint8_t cnt,		/* Read byte count */
+	uint8_t *buff		/* Read data buffer */
+);
 
 BOOL rtc_init (void);						/* Initialize RTC */
 BOOL rtc_gettime (RTC*);					/* Get time */
