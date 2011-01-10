04859c752a45eeaa04ca42cc86a51be6fd2fd8fb
spyfluen_mj@hotmail.com <spyfluen_mj@hotmail.com@cee7091a-c234-0410-ab42-b1a86514ffa4>
2011-01-10 23:30:41 +0000

    Startet på at skrive nyt can kode

--- car_software/g5/node5/can_func.h
+++ car_software/g5/node5/can_func.h
@@ -1,3 +1,6 @@
+#include "can_logning.h"
+#include "integer.h"
+
 #define num_of_response_mobs 2
 #define data_buffer_num 2 
 
@@ -10,10 +13,15 @@ extern U8 bufferindex;
 extern U8 tx_remote_buffer[8];
 extern st_cmd_t tx_remote_msg;
 
+extern U8 tx_buffer[8];
+extern st_cmd_t tx_std_msg;
+
 void init_can_data_mobs(void);
 void can_data_mob_setup(U8 mob_num);
 
 /* Funktion til at sende en besked via CAN */
+BOOL can_send_standart_data(dataPackage *, U8 *buf);
+
 void can_send(U8 msg_id, U8 msg, U8 data_length);
 
 void can_send_test(U8 msg_id, void* msg, U8 data_length);
