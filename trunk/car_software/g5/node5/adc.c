41557705d3f49fd57b8f89965e6ef825caf4ba7d
spyfluen_mj@hotmail.com <spyfluen_mj@hotmail.com@cee7091a-c234-0410-ab42-b1a86514ffa4>
2011-01-30 12:30:05 +0000

    Added code to make adc conversion. Code marked with /* NEW CODE */ need to be tested.

--- trunk/car_software/g5/node5/adc.c
+++ trunk/car_software/g5/node5/adc.c
@@ -5,37 +5,59 @@
 #include "../lib/can_defs.h"
 #include "adc.h"
 
-ADCReadObject ADCReadObjects[] = {	{Temp1, 2000, 2000, 0}, 
-					{Temp2,2500, 2500, 0}, 
-					{Temp3, 2700, 2700, 0}};	
+ADCReadObject ADCReadObjects[] = {	{ADC0, 2000, 2000, 0}, 
+					{ADC1,2500, 2500, 0}, 
+					{ADC2, 2700, 2700, 0}};	
+
+/* Used together with ADCReadObjectBuff */
+uint8_t ADCReadObjectBuffSize = 0;
+uint8_t ADCReadObjectBuffHead = 0;
 
 /* Timer1 overflow interrupt used for adc read function */
 ISR (TIMER1_OVF_vect)
 {
 	uint8_t i;
+	ADCReadObject *P1;
+	/* test vars */
 	uint8_t data_buf[8];
 	data_buf[0] = 1;
 	data_buf[1] = 120;
+	/* test vars stop */
 
 	ADC_TIMER_TOID;		/* Disable timer interrupt */ 
 	
-
 	/* Count timeout for all items in queue down by timeout for item 
 	 * first i queue */
 	CountDown();
 
-	/* TODO start conversion for items with timeout at zero or less */
+	/* NEW CODE */
+	/* Start conversion for items with timeout at zero */
+	P1 = QH;
+	ADCReadObjectBuffSize = 0;
+	while (P1->timeout == 0){	/* for alle Objects with timeout == 0 */
+		ADCReadObjectBuff[ADCReadObjectBuffSize] = P1;
+		ADCReadObjectBuffSize++;
+		P1 = P1->next;
+	}
+	
+	MakeADCReadObjectConversion(); /* Start conversion. Next objects will
+					* will be called from ADC conversion 
+   					* done interrupt */
+	/* NEW CODE END*/
 
+	
 	/* Remove items with timeout at zero or less and put them back into
 	 * queue at the correct place */
 	while(QH->timeout == 0){
 		ReloadQueue();
 	}
 	
+	/* test printing */
 	data_buf[0] = QH->adc;
 	data_buf[1] = QH->interval/20;
 	data_buf[2] = QH->timeout/20;		
 	can_send_ny(rpm_msgid, data_buf, 8);
+	/* test printing stop */
 
 	/* Load timer with value of timeout for the first item in the queue */
 	SetTimer(QH->timeout);
@@ -45,14 +67,44 @@ ISR (TIMER1_OVF_vect)
 
 /* ADC conversion done interrupt */
 ISR (ADC_vect)
-{
+{	
+
 	/* TODO
 	 * Figure out some way to to arrange data from the conversion in
 	 * accordans with the can data packaged definitions. 
 	 * Need some way to describe the relation between ADCReadObjects
 	 * and can data packeges  
-	*/  
+	*/
+	
+	/* NEW CODE */
+	/* After date is saved for last conversion start new conversion if
+	 * there is anymore ADCReadObjects to make conversion for */
+	MakeADCReadObjectConversion();  
+	/* NEW CODE END */
 }
+
+/* NEW CODE */
+void InitADC(void)
+{
+	SET_ADC_PRESCALER;	/* Set prescaler */
+	SET_ADC_REF;		/* Set ADC conversion reference */
+	EN_ADC;			/* Enable ADC */
+	ADC_IE;			/* Enable ADC conversion done interrupt */
+}
+
+void MakeADCReadObjectConversion(void)
+{
+	if (ADCReadObjectBuffSize == 0){/* If no more Objects reset Head */ 
+		ADCReadObjectBuffHead = 0;
+	}else{
+		/* Select ADC channel for conversion */
+		Set_adc_mux( ADCReadObjectBuff[ADCReadObjectBuffHead]->adc );
+		ADCReadObjectBuffHead++;
+		ADCReadObjectBuffSize--;
+		ADC_START_CONV;		/* Start conversion */
+	}
+}
+/* NEW CODE END*/
 
 
 void AdcReadStart(void)
@@ -60,6 +112,8 @@ void AdcReadStart(void)
 	int i;
 	ADCReadObject *P1;	
 
+	InitADC;	/* Initialize ADC in accordance with definitions made in 
+			 * adc.h */
 
 	SET_ADC_TIMER_PRESCALER;	/* Start timer 1 but no interrup yet */
 
